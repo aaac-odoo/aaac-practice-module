f338f2cbec59850777e11acdae9c1e633cc51e8f
Aamir Achhava <aaac@gmail.com>
2023-01-27 18:57:17 +0530

    [ADD] stockbroker: implemented order list in user module
    implemented order  in user module to fetch users according to their growth in
    account balance to get highest performing users at top

--- stock_broker/models/user.py
+++ stock_broker/models/user.py
@@ -4,10 +4,15 @@ from . import order
 class listedCompany(models.Model):
     _name = "users"
     _description="model description"
+    _order="growth desc"
 
     name=fields.Char(string="Name", required=True)
     initial_balance=fields.Float(string="Initial Balance", required=True)
     current_balance=fields.Float(string="Current Balance", required=True)
-
+    growth=fields.Float(string="Growth %", compute='_compute_growth', store=True)
+    @api.depends('initial_balance','current_balance')
+    def _compute_growth(self):
+        for record in self:
+            record.growth=((record.current_balance - record.initial_balance)/record.current_balance)*100
 
     order_ids=fields.One2many("orders","user",string="Orders")
